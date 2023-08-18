b63ef01e3c7104a70dd34034e924c52175c4547e
Martin Maca <maca@intemac.cz>
2023-08-18 15:30:25 +0000

    cleaned up IO_control class, better readability, better arranged

--- class_IO_control.h
+++ class_IO_control.h
@@ -10,71 +10,22 @@ class IO_control {
         IO_control ();
         IO_control (int type_number, int group_number, int io_number);
         void initialize_IO (int type_number, int group_number, int io_number);
-
         void set_output (bool status);
-        bool get_status (){
-            string status;
-            ifstream stat (address);
-            getline (stat,status);
-            bool io_state;
-            if (status == "1"){
-                io_state = true;
-            }else if (status == "0"){
-                io_state = false;
-            }else{
-                print_me ("error input status");
-            }
-            return io_state;
-        }
-        void set_high (){
-            if (initialized == true){
-                ofstream output_control (address);
-                output_control << "1";
-                print_me(address+" - 1");
-                output_control.close();
-            } else {
-                print_me ("this IO has not been initialized");
-            }
-        }
-        void set_low (){
-            if (initialized == true){
-                ofstream output_control (address);
-                output_control << "0";
-                print_me (address+" - 0");
-                output_control.close();
-            } else {
-
-            }
-        }
+        bool get_status ();
+        void set_high ();
+        void set_low ();
 
     private: 
         int type_num, group_num, io_num;
         string IO_name = "not initialized";
         string address = "not initialized";
         bool initialized = false;
-        string get_io_address (){
-            string Sio_type = get_io_type ();
-            string Sgroup_number = to_string (group_num);
-            string Sio_num = to_string (io_num);
-            string register_address = "/run/unipi/io_group" + Sgroup_number + "/" + Sio_type + "_" + Sgroup_number + "_0" + Sio_num + "/" + Sio_type + "_value";
-            print_me (register_address);
-            return register_address;
-        }
-        string get_io_type (){
-            const static string type_array []= {"di","do","ro"};
-
-            string io_type = type_array [type_num];
-            
-            return io_type;
-        }
-        string get_IO_name (){
-            
-            return IO_name;
-        }
-        void print_me (string to_be_printed){
-            cout << to_be_printed + "\n";
-        }
+        string get_io_address ();
+        string get_io_type ();
+        string get_IO_name ();
+        void print_me (string to_be_printed);
 };
+
 IO_control::IO_control(){
     
 }
@@ -96,5 +47,65 @@ void IO_control::set_output(bool status){
         set_low ();
     }
 }
+
+
+bool IO_control::get_status (){
+    string status;
+    ifstream stat (address);
+    getline (stat,status);
+    bool io_state;
+    if (status == "1"){
+        io_state = true;
+    }else if (status == "0"){
+        io_state = false;
+    }else{
+        print_me ("error input status");
+    }
+    return io_state;
+}
+void IO_control::set_high (){
+    if (initialized == true){
+        ofstream output_control (address);
+        output_control << "1";
+        print_me(address+" - 1");
+        output_control.close();
+    } else {
+        print_me ("this IO has not been initialized");
+    }
+}
+void IO_control::set_low (){
+    if (initialized == true){
+        ofstream output_control (address);
+        output_control << "0";
+        print_me (address+" - 0");
+        output_control.close();
+    } else {
+
+    }
+}
+string IO_control::get_io_address (){
+    string Sio_type = get_io_type ();
+    string Sgroup_number = to_string (group_num);
+    string Sio_num = to_string (io_num);
+    string register_address = "/run/unipi/io_group" + Sgroup_number + "/" + Sio_type + "_" + Sgroup_number + "_0" + Sio_num + "/" + Sio_type + "_value";
+    print_me (register_address);
+    return register_address;
+}
+string IO_control::get_io_type (){
+    const static string type_array []= {"di","do","ro"};
+
+    string io_type = type_array [type_num];
+    
+    return io_type;
+}
+string IO_control::get_IO_name (){
+    
+    return IO_name;
+}
+void IO_control::print_me (string to_be_printed){
+    cout << to_be_printed + "\n";
+}
+
+
 
 #endif
