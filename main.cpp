bb37519ab5c2dc79eab98c8e88768e26070337ca
Martin Maca <maca@intemac.cz>
2023-10-02 10:43:20 +0000

    tuning stepper to do 360 degrees

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std::this_thread;
 using namespace std::chrono;
 
 void test_motor_enable_bit();
-void rotate_stepper();
+void rotate_stepper(int turn_degrees);
 void enable_motor_control();
 
 int main (){
@@ -14,10 +14,11 @@ int main (){
 
   //  test_motor_enable_bit();
     enable_motor_control();
-    rotate_stepper();
-    outputs [2].set_high();
-    rotate_stepper();
 
+  //  for (int i=1; i<=20; i++){
+        rotate_stepper(10);
+        my_delay(2);
+ //   }
     printf ("\n END program main  \n");
     return 0;
 }
@@ -42,7 +43,7 @@ void test_motor_enable_bit (){
     enable_motor_control();
     my_delay(4);
 }
-
+/*
 void rotate_stepper (){
     for (int i=1; i<10000; i++){  //100x pulse
         outputs[3].set_high(); 
@@ -51,13 +52,16 @@ void rotate_stepper (){
         sleep_for(microseconds (100));
     }
 }
-
-void rotate_stepper (int degrees){
-    int mimimum_pulse_delay = 100;
-    for (int i=1; i<200; i++){
+*/
+void rotate_stepper (int turn_degrees){
+    int mimimum_pulse_delay = 10;
+    int number_of_pulses = 0;
+    for (int i=1; i<=800; i++){
         outputs[3].set_high();
         sleep_for(microseconds (mimimum_pulse_delay));
         outputs[3].set_low();
         sleep_for(microseconds (mimimum_pulse_delay));
+        number_of_pulses ++;
     }
+    cout<< number_of_pulses;
 }
