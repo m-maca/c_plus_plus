eb222ff5b3b3e353d09cb826927075300cb26873
Martin Maca <maca@intemac.cz>
2023-10-02 07:17:10 +0000

    new program for testing a stepper motor

--- main.cpp
+++ main.cpp
@@ -4,29 +4,49 @@ using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
 
+void test_motor_enable_bit();
+void rotate_stepper();
+
 
 int main (){
     printf  ("\n here the program main starts\n");
     initialize_all_IOs();
-    outputs[1].set_high();
-    sleep_for (milliseconds (400)); //enable
-    outputs[2].set_high(); //direction
-    sleep_for (microseconds (100));
 
-    
-    
-    
-    my_delay(4);
-   // outputs[1].set_low();
+    test_motor_enable_bit();
+
+    rotate_stepper();
 
 
-    for (int i=1; i<100; i++){  //100x pulse
-        outputs[3].set_high(); 
-        sleep_for(microseconds (50));
-        outputs[3].set_low();
-        sleep_for(microseconds (50));
-        
-    }
     printf ("\n END program main  \n");
     return 0;
-};
+}
+
+void enable_motor_control (){
+    outputs [1].set_low();
+    printf ("/n motor enabled");
+    sleep_for (milliseconds (400));
+}
+
+void disable_motor_control (){
+    outputs [1].set_high();
+    printf ("/n motor disabled");
+    sleep_for (milliseconds (400));
+}
+
+void test_motor_enable_bit (){
+    enable_motor_control();
+    my_delay (4);
+    disable_motor_control();
+    my_delay(4);
+    enable_motor_control();
+    my_delay(4);
+}
+
+void rotate_stepper (){
+    for (int i=1; i<100; i++){  //100x pulse
+        outputs[3].set_high(); 
+        sleep_for(microseconds (100));
+        outputs[3].set_low();
+        sleep_for(microseconds (100));
+    }
+}
